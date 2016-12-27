20dd4209e970b01b138913465ad52ca820a6a439
Adam-D-Sanchez <atozadam@gmail.com>
2016-12-26 19:09:08 -0800

    Update Controller_tb.v
    Removed unnecessary text.

--- Lab8Division.srcs/sim_1/new/Controller_tb.v
+++ Lab8Division.srcs/sim_1/new/Controller_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/21/2016 03:10:38 PM
-// Design Name: 
-// Module Name: Controller_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Controller_tb(
 
     );

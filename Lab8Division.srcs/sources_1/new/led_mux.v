c5181ad82e200b22c76994accc2e9b6494d0de67
Adam-D-Sanchez <atozadam@gmail.com>
2016-12-26 19:28:52 -0800

    Update led_mux.v
    Removed unnecessary heading.

--- Lab8Division.srcs/sources_1/new/led_mux.v
+++ Lab8Division.srcs/sources_1/new/led_mux.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/28/2016 01:56:58 PM
-// Design Name: 
-// Module Name: led_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module led_mux (  
 input wire clk,  
 input wire rst,

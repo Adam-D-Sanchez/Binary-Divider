f976414c208449f3c8ec5cb5fd445db7806fa27c
Adam-D-Sanchez <atozadam@gmail.com>
2016-12-26 19:19:32 -0800

    Update bigmux.v
    Removed unnecessary header.

--- Lab8Division.srcs/sources_1/new/bigmux.v
+++ Lab8Division.srcs/sources_1/new/bigmux.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/20/2016 04:31:46 PM
-// Design Name: 
-// Module Name: bigmux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bigmux( input sel1,
 	input [4:0] in0, in1,
 	output reg [4:0] out

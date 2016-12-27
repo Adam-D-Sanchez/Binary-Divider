ce295441ab080e06543c46e783f5d50ae89d39a3
Adam-D-Sanchez <atozadam@gmail.com>
2016-12-26 19:23:21 -0800

    Update bigshiftdreg.v
    Removed unnecessary heading. 
    Added some comments.

--- Lab8Division.srcs/sources_1/new/bigshiftdreg.v
+++ Lab8Division.srcs/sources_1/new/bigshiftdreg.v
@@ -1,35 +1,13 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/20/2016 04:35:02 PM
-// Design Name: 
-// Module Name: bigshiftdreg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bigshiftdreg(input clk, rst, ld, sl, sr, en, lb, rb,
 	input[4:0] d_in,
 	output reg [4:0] q_out
     );
 
-always@(posedge clk) begin
-		 if (rst) q_out<=5'b0;
-	else if (ld) q_out<=d_in; 
-	else if (sr&&en)q_out<={lb,q_out[4:1]}; 
-	else if (sl&&en) q_out<={q_out[3:0],rb};
-	else q_out<=q_out;
+always@(posedge clk) begin //synchronous.
+	if (rst) q_out<=5'b0; //reset asserted
+	else if (ld) q_out<=d_in;  //load asserted
+	else if (sr&&en)q_out<={lb,q_out[4:1]}; //allows shifting to the right. 
+	else if (sl&&en) q_out<={q_out[3:0],rb}; //allows shifting to the left.
+	else q_out<=q_out; //holds if no other signals are active.
 end
 endmodule
